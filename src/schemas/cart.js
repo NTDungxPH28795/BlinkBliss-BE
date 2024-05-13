824522440aea92eba89f1fdbdd94d60eba6b1bf4
NTDungxPH28795 <dungndtph28795@fpt.edu.vn>
2024-05-13 21:35:08 +0700

    feat: payment

--- src/schemas/cart.js
+++ src/schemas/cart.js
@@ -1,6 +1,5 @@
 import { cartDetailSchema } from "./cartDetail";
 
-
 export const cartSchema = Joi.object({
     user_id: Joi.string().required(),
     products: Joi.array().items(cartDetailSchema),
