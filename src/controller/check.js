e6e0e50e22e19d3ace39910111b2eb5a8c51e16e
QMinh <127032061+JunMinh@users.noreply.github.com>
2024-05-14 19:54:38 +0700

    Update check.js

--- src/controller/check.js
+++ src/controller/check.js
@@ -188,3 +188,21 @@ export const updateCheckout = async (req, res) => {
         });
     }
 };
+function sortObject(obj) {
+    let sorted = {};
+    let str = [];
+    let key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            str.push(encodeURIComponent(key));
+        }
+    }
+    str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(
+            /%20/g,
+            '+'
+        );
+    }
+    return sorted;
+}
